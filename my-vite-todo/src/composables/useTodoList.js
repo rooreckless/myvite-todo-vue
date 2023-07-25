fa7fd4d24746c0c52628897e2fb3dd3e68870781
rooreckless <rooreckless@gmail.com>
2023-07-25 15:27:58 +0900

    01 initial

--- my-vite-todo/src/composables/useTodoList.js
+++ my-vite-todo/src/composables/useTodoList.js
@@ -36,7 +36,10 @@ export const useTodoList=()=>{
     console.log("--onmount_alltask--")
     try{
       const res = await fetch("/back_app/alltask")
-      console.log("--onmount_alltask--res="+res)
+      console.log("--onmount_alltask--res=")
+      console.log(res)
+      console.log("res.json()=")
+      console.log(res.json())
       //fetch結果の本文のみ抽出しJSON化
       //→その中のtodos_listだけを取り出してreturnしPromiseResultに詰める
       const resjson=await res.json()
